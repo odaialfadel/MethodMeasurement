bdeea471bd65144b70caa631bae94932081c0485
Odai Al Fadel <odai.alfadel@hotmail.com>
2022-10-22 21:54:22 +0200

    use example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Monitoring moni = new Monitoring();
+        moni.start();
+        loop();
+        moni.stop();
     }
+
+    static void loop(){
+        for(int i = 0; i<=1000_000; i++){
+            System.out.println(i);
+        }
+    }
+
 }
